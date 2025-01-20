8596af8f4ff47e4ff77c4bc6222fd8dc2d084633
KOUTAN-App-Builders <gikou21doc@gmail.com>
2025-01-20 18:16:01 +0900

    Initial Commit

---
+++ MeDex for Mac/MeDex_for_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeDex_for_MacApp.swift
+//  MeDex for Mac
+//
+//  Created by 加納塙大 Editor on 2025/01/20.
+//
+
+import SwiftUI
+
+@main
+struct MeDex_for_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
