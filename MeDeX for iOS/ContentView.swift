dd24282332021e155c4aaf030c6240d689fcef1d
KOUTAN-App-Builders <gikou21doc@gmail.com>
2025-01-21 23:56:47 +0900

    Initial Commit

---
+++ MeDeX for iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeDeX for iOS
+//
+//  Created by 加納塙大 Editor on 2025/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeDeX for iOS/MeDeX_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeDeX_for_iOSApp.swift
+//  MeDeX for iOS
+//
+//  Created by 加納塙大 Editor on 2025/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MeDeX_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
