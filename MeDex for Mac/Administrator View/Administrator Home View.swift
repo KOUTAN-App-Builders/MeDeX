3e039c6791a382d040aa5c5ff8997c53784f0e8a
KOUTAN-App-Builders <gikou21doc@gmail.com>
2025-01-24 19:26:25 +0900

    Made some UI changes to the Mac App. Functionalities aren't added yet. Navigation Links between Views are available.

---
+++ MeDex for Mac/Administrator View/Administrator Home View.swift
@@ -0,0 +1,18 @@
+//
+//  Administrator Home View.swift
+//  MeDex for Mac
+//
+//  Created by 加納塙大 Editor on 2025/01/24.
+//
+
+import SwiftUI
+
+struct Administrator_Home_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Administrator_Home_View()
+}
