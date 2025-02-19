5671ff22ff1b8e890f430f948788e012a7240be1
KOUTAN-App-Builders <gikou21doc@gmail.com>
2025-02-19 15:48:41 +0900

    Syncing previous UI changes. Updated the changes made for the UI. Will be making further changes in the next few days.

--- Package.swift
+++ Package.swift
@@ -4,5 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "Data Manager for MeDeX"
+    name: "Data Manager for MeDeX",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17),
+    ],
+    products: [
+        .library(
+            name: "Data Manager for MeDeX",
+            targets: ["Data Manager for MeDeX"]),
+    ],
+    dependencies: [
+        
+    ],
+    targets: [
+        .target(
+            name: "Data Manager for MeDeX",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "Data Manager for MeDeXTests",
+            dependencies: ["Data Manager for MeDeX"]),
+    ]
 )
---
+++ Tests/Data Manager for MeDeXTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Data Manager for MeDeX
+//
+//  Created by 加納塙大 Editor on 2025/01/25.
+//
+
+import Foundation
